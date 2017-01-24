af65dcda4535ae511cfd38f886348e0abf6bee7b
Elixandre Michael <Elixandre Michael>
2017-01-24 10:12:25 -0200

    rotas atualizadas

--- app/Http/Controllers/CursosController.php
+++ app/Http/Controllers/CursosController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class CursosController extends Controller
 {
-    //
+    //protected $redirectTo = '/cursos'; PRECISA??
+
+    public function show()
+    {
+    	return view('/cursos');
+    }
 }
