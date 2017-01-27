e3db2416ba51daed187586be8e96cd5e7d1871f4
Luiz <luizguilhermefr@gmail.com>
2017-01-27 10:02:11 -0200

    atuando na exclusão de entidades

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,16 @@
-function delete(id, entity){
-	if (confirm("Tem certeza que deseja efetuar esta exclusão?")) {
-		$.ajaxSetup({
-			headers: {
-        		'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    		}
+var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+
+function delete (id, entity_url) {
+	// exemplo: entity_url = '/professores/'
+	if (confirm("Tem certeza de que deseja efetuar esta exclusão?")) {
+			$.ajax({
+	    	url: entity_url + id,
+	    	type: 'POST',
+	    	data: {_token: CSRF_TOKEN},
+	    	dataType: 'JSON',
+	    	success: function (data) {
+	        console.log(data);
+	    	}
 		});
-        $.ajax({
-			type: "POST",
-			url: entity + id + "/excluir",
-			success: function (data) {
-				alert(data);
-			},
-		});
-  }
+	}
 }
